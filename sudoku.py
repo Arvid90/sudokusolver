172c385ab7e3d3929c2dfdfc61f522486e89f64f
Arvid <ArvidNilsson_90@hotmail.com>
2020-07-19 15:13:04 +0200

    changed a lot of things

--- sudoku.py
+++ sudoku.py
@@ -1,10 +1,8 @@
-#sudoku
+# sudoku
 
-
-
-Sbox = [[1, 2, 3, 4, 5, 6, 7, 8, 0],
+Sbox = [[1, 2, 3, 3, 5, 6, 7, 8, 0],
         [2, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 3, 0, 0, 0, 0, 0, 0],
+        [3, 0, 3, 0, 0, 0, 0, 0, 0],
         [0, 0, 0, 0, 0, 0, 0, 0, 0],
         [0, 0, 0, 0, 0, 0, 0, 0, 0],
         [0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -12,21 +10,44 @@ Sbox = [[1, 2, 3, 4, 5, 6, 7, 8, 0],
         [0, 0, 0, 0, 0, 0, 0, 0, 0],
         [0, 0, 0, 0, 0, 0, 0, 0, 0]]
 
+
 def test_row(row, guess):
     for i in range(9):
         print("guess: ", guess, "pos: ", Sbox[row][i])
-        if guess==Sbox[i][row]:
+        if guess == Sbox[row][i]:
+            print("collision")
             return False
-        return True
+    print("possibruh")
+    return True
 
-#        def test_collumn(index, guess)
-#        def test_subsquare(index, guess)
-#        def test_nextsquare
 
-#print("testing row ", Sbox[0])
-#for guess in range(9):
-#    print("testing guess ", guess)
-    if (test_row(0, guess)):
-        print ("possibruh")
+def test_column(column, guess):
+    for i in range(9):
+        print("Guess: ", guess, "At pos: ", Sbox[i][column])
+        if guess == Sbox[i][column]:
+            print("collision")
+            return False
+    print("possibruh")
+    return True
+
+
+def test_subsquare(row, col, guess):
+    #solve with math instead of branching?
+    if row < 3:
+        #1
+    elif row < 6:
+        ##2
     else:
-        print("collision")
+        #3
+
+#        def test_next
+
+#print("testing col ", Sbox[][0])
+test_col = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+for i in range(9):
+    test_col[i] = Sbox[i][0]
+print("testing column: ", test_col)
+test_column(0, 4)
+#for guess in range(9):
+#    print("testing guess: ", guess+1)
+#    test_row(0, guess+1)
