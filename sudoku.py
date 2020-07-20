61eb33f4727ba31098c1f4a867ec921811d2c2ad
Arvid <arvidnilsson_90@hotmail.com>
2020-07-20 18:34:07 +0200

    graphical representation of process somewhat operational. Not interactive yet, nor speedy

--- sudoku.py
+++ sudoku.py
@@ -1,4 +1,31 @@
 # sudoku
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def sdraw(fig, ones, l):
+    plt.clf()
+    ax = fig.add_subplot(111)
+    ax.matshow(ones)
+    for i in range(9):
+        for j in range(9):
+            if int(l[i, j]) == 0:
+                ax.text(i, j, " ", ha="center", va="center", color="w")
+            else:
+                ax.text(i, j, int(l[i, j]), ha="center", va="center", color="w")
+    plt.draw()
+    plt.pause(0.01)
+
+#for i in range(0, N):
+#    lattice[i][i] = i+1
+#    ax = fig.add_subplot(111)
+#    ax.matshow(ones)
+#    ax.text(i, i, int(lattice[i, i]), ha="center", va="center", color="w")
+#    plt.draw()
+#    plt.pause(0.5)
+
+
+
 
 Sbox = [[0, 0, 6, 8, 0, 2, 9, 0, 7],
         [1, 0, 0, 9, 0, 4, 0, 0, 0],
@@ -37,6 +64,7 @@ s = \
         [3, 7, 8, 1, 2, 5, 9, 4, 6],
         [5, 4, 6, 3, 8, 9, 7, 2, 1],
         [1, 2, 9, 7, 6, 4, 5, 3, 8]]
+
 
 
 def test_row(row, guess):
@@ -81,21 +109,26 @@ def is_valid(s,i,j,k):
 def find_nonempty(s):
     for i in range(9):
         for j in range(9):
-            if s[i][j] == 0:
-                return (i,j)
+            if s[j][i] == 0:
+                return (j, i)
     print("error in find_nonempty")
 
 def solve(s):
-    for l in range(9):
-        print(s[l])
-    print()
+#    for l in range(9):
+#        print(s[l])
+#    print()
     if is_full(s):
+        plt.pause(5)
         return True
     else:
         (i, j) = find_nonempty(s)
     for k in range(1, 10):
         if is_valid(s, i, j, k):
             s[i][j] = k
+            for ii in range(9):
+                for jj in range(9):
+                    lattice[ii][jj] = s[ii][jj]
+            sdraw(fig, ones, lattice)
             if solve(s):
                 return True
 #    print(s)
@@ -107,5 +140,11 @@ def solve(s):
 #output = is_full(Sbox)
 #output = find_nonempty(Sbox)
 #print (output)
-output = solve(s)
-print(output)
+
+N = 9
+lattice = np.ones([N, N])
+ones = np.ones([N, N])
+plt.ion() #turns on interactive mode
+fig = plt.figure()
+ax = fig.add_subplot(111)
+output = solve(Sbox)
